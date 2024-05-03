8113030ae6a08a4ca4983d8d6579039425cfd1f2
Salomon BRYS <salomon.brys@gmail.com>
2024-05-03 17:35:14 +0200

    2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
-import org.jetbrains.kotlin.com.google.gson.Gson
-import org.jetbrains.kotlin.com.google.gson.JsonDeserializer
-import org.jetbrains.kotlin.com.google.gson.JsonParser
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse.BodyHandlers
-
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
@@ -24,7 +16,7 @@ subprojects {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/kodeinkoders/kodein-themes")
                 credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                     password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
                 }
             }
